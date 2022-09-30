26aa40cfc450cd0236b46574120894ade33dc578
Daniifer18 <danportero6334@gmail.com>
2022-09-30 21:35:22 +0200

    fin

--- Inicio/E1.js
+++ Inicio/E1.js
@@ -8,8 +8,14 @@ if(edad > 18){
     console.log("Es mayor de edad");
 }
 
+
 for (let i = 0; i < 11; i++) {
-   if(i%2==0){
+    if(i%2==0){
         console.log(i);
-   }
+    }
 }
+let arr = ["d","a","n","i","e","l"];
+let arr1 = ["daniel"];
+
+console.log(arr[0].substring(2,3));
+console.log(arr2[0].substring(2,3));
