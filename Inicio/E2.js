f85d239db4778f8f2b2778bb1ca5607f8e0609d6
Daniifer18 <danportero6334@gmail.com>
2022-10-03 17:45:01 +0200

    fn

--- Inicio/E2.js
+++ Inicio/E2.js
@@ -2,7 +2,7 @@ const encender = document.getElementById("der");
 const apagar = document.getElementById("izq");
 const foto = document.getElementById("foto");
 
-encender.onclick = a => foto.src = "encendida.png";
+encender.onclick = evento => foto.src = "encendida.png";
 
-apagar.onclick = b => foto.src = "apagada.png";
+apagar.onclick = evento => foto.src = "apagada.png";
 
