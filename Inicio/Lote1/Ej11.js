bc6bc1337a4b5e87c205525a401ee8828da40e51
Daniifer18 <danportero6334@gmail.com>
2022-10-15 13:32:39 +0200

    fin

---
+++ Inicio/Lote1/Ej11.js
@@ -0,0 +1,29 @@
+function random(min, max) {
+
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+
+}
+
+let numSecreto = parseInt(prompt("Introduzca el número secreto"));
+
+
+let numMaquina = random(0,100);
+let valorVariable = 0;
+let numIntentos = 0;
+
+valorVariableMin = numMaquina;
+
+while(numMaquina != numSecreto){
+    if(numMaquina > numSecreto){
+        document.write("<h3>El número secreto es menor.</h3>");
+        valorVariable = numMaquina;
+        numMaquina = random(0,valorVariable);
+    }else{
+        document.write("<h3>El número secreto es mayor.</h3>");
+        valorVariable = numMaquina;
+        numMaquina = random(valorVariable,100);
+    }
+    numIntentos++;
+}
+
+document.write("<h1>La maquina encontró el número secreto ("+numSecreto+") en "+numIntentos+" intentos</h1>");
