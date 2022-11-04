889383a00b1d1867ce3d1a56f0f85aa78555417a
Daniifer18 <danportero6334@gmail.com>
2022-11-04 21:13:13 +0100

    fin

---
+++ Inicio/Tema 3/3.4/Ej1.js
@@ -0,0 +1,17 @@
+let v;
+
+function abrirVentanaNueva(){
+
+
+    if (confirm("¿Está de acuerdo con esto?") == true) {
+
+        v = window.open("https://www.w3schools.com/js/default.asp" ,"Comunicacion de ventanas" ,"width=300px, height=200px");
+
+        v.moveBy(500,500);
+
+        v.moveTo(100,200);
+
+        v.scrollTo(0, v.innerHeight());
+    }
+
+}
---
+++ Inicio/Tema 3/3.4/Ej2.js
@@ -0,0 +1,23 @@
+let ventana;
+
+function abrir(){
+
+    let num = parseInt(Math.random()* 3);
+
+
+    switch (num) {
+        case 0:
+            ventana = window.open("https://mail.google.com/" ,"Comunicacion de ventanas" ,"width=600, height=500");
+            break;
+        case 1:
+            ventana = window.open("https://correoweb.educa.madrid.org/" ,"Comunicacion de ventanas" ,"width=600, height=500");
+            break;
+        case 2:
+            ventana = window.open("https://es.yahoo.com/" ,"Comunicacion de ventanas" ,"width=600, height=500");
+            break;
+        case 3:
+            ventana = window.open("https://outlook.live.com/owa/" ,"Comunicacion de ventanas" ,"width=600, height=500");
+        break;
+    }
+
+}
