57eb2374565c7610caeaedea3adb59a87bef2cef
Daniifer18 <danportero6334@gmail.com>
2022-11-04 19:48:55 +0100

    fin

--- Inicio/Tema 3/3.3/ventana.js
+++ Inicio/Tema 3/3.3/ventana.js
@@ -1,17 +1,17 @@
+let vs;
 
 function abrirVentana(){
     
-    let vs = window.open("https://www.w3schools.com/js/default.asp" ,"Comunicacion de ventanas" ,"width=600, height=500");	
+    vs = window.open("https://www.w3schools.com/js/default.asp" ,"Comunicacion de ventanas" ,"width=600, height=500");	
     
-
-    setTimeout(()=>
-        vs.close()
-    ,10000);
- 
 }
 
 function cerrarVentana(){
 
     vs.close();
+
+    setTimeout(()=>
+        vs.close()
+    ,10000);
     
 }
